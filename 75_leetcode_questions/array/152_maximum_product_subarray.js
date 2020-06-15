ef04a29bf4df995f9cccb638d39808556af687dd
Isaac Nam <ikn208@nyu.edu>
2020-06-15 15:05:58 -0400

    maximum product

---
+++ 75_leetcode_questions/array/152_maximum_product_subarray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// in progress
+
+var maxSubArray = function(nums) {
+    let max = nums[0];
+    let currentSum = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        if ((nums[i] + currentSum) > nums[i]) {
+            currentSum += nums[i];
+            if (currentSum > max) {
+                max = currentSum;
+            }
+        } else {
+            currentSum = nums[i];
+            if (currentSum > max) {
+                max = currentSum;
+            }
+        }
+    }
+    
+    return max;
+};
