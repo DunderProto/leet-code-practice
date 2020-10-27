6bcdf35faf9e85445e5d109a947a7aae6c2b28e7
Isaac Nam <ikn208@nyu.edu>
2020-10-27 18:11:52 -0400

    find minimum in rotated sorted array

--- 75_leetcode_questions/array/153_find_minimum_in_rotated_sorted_array.js
+++ 75_leetcode_questions/array/153_find_minimum_in_rotated_sorted_array.js
@@ -14,6 +14,48 @@
 
 
 var findMin = function(nums) {
-    
+    if (nums.length == 0) return -1;
+    if (nums.length == 1) return nums[0];
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let midpoint = Math.floor((left + right) / 2);
+        if (midpoint > 0 && nums[midpoint] < nums[midpoint - 1]) {
+            return nums[midpoint];
+        } else if (nums[midpoint] < nums[left] && nums[midpoint] <= nums[right]) {
+            right = midpoint - 1;
+        } else {
+            left = midpoint + 1;
+        }
+    }
+
+    return nums[left];
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    if (nums.length == 0) return -1;
+    if (nums.length == 1) return nums[0];
+
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let midpoint = Math.floor((left + right) / 2);
+        if (midpoint > 0 && nums[midpoint] < nums[midpoint - 1]) {
+            return nums[midpoint];
+        } else if (nums[left] <= nums[midpoint] && nums[midpoint] > nums[right]) {
+            left = midpoint + 1;
+        } else {
+            right = midpoint - 1;
+        }
+    }
+
+    return nums[left];
 }
 
