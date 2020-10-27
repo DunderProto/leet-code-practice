d62722558b01b9227facd4fe00956696f004b103
Isaac Nam <ikn208@nyu.edu>
2020-10-27 17:42:52 -0400

    find min in sorted array

--- 75_leetcode_questions/array/153_find_minimum_in_rotated_sorted_array.js
+++ 75_leetcode_questions/array/153_find_minimum_in_rotated_sorted_array.js
@@ -2,14 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
-    let min = Infinity;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] < min) {
-            min = nums[i];
-        }
-    }
-    return min;
-};
+// var findMin = function(nums) {
+//     let min = Infinity;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] < min) {
+//             min = nums[i];
+//         }
+//     }
+//     return min;
+// };
 
-console.log("yo");
+
+var findMin = function(nums) {
+    
+}
+
