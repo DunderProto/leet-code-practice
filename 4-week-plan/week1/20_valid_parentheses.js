a4cdb7057e10d0571396efe8f526a5c968693708
Isaac Nam <ikn208@nyu.edu>
2020-10-31 16:18:18 -0400

    valid parentheses

--- 4-week-plan/week1/20_valid_parentheses.js
+++ 4-week-plan/week1/20_valid_parentheses.js
@@ -23,3 +23,28 @@ var isValid = function(s) {
         return false;
     }
 };
+
+// FASTER SOLUTION
+var isValid = function (s) {
+
+    const obj = {
+      "(": ")",
+      "{": "}",
+      "[": "]",
+    }
+  
+    const stack = [];
+  
+    for (const paran of s) {
+      if (obj.hasOwnProperty(paran)) {
+        stack.push(paran)
+      } else {
+        const closeParan = stack.pop();
+        if (paran !== obj[closeParan]) {
+          return false;
+        }
+      }
+    }
+  
+    return stack.length === 0;
+  };
