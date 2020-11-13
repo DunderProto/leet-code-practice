84ff628367c06bc445f0415d2a5791a524bad78e
Isaac Nam <ikn208@nyu.edu>
2020-11-12 22:02:49 -0500

    coin change

--- 4-week-plan/week3/322_coin_change.js
+++ 4-week-plan/week3/322_coin_change.js
@@ -26,3 +26,25 @@ var coinChange = function(coins, amount) {
     
     return arr[arr.length - 1];
 };
+
+// ACCEPTED SOLUTION
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let arr = new Array(amount + 1);
+    arr.fill(amount + 1);
+    arr[0] = 0;
+    
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < coins.length; j++) {
+            if (coins[j] <= i) {
+                arr[i] = Math.min(arr[i], arr[i - coins[j]] + 1);
+            }
+        }
+    }
+    
+    return arr[amount] > amount ? -1 : arr[amount];
+};
